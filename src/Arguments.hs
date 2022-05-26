7e9b7382ebf3028005fcd1023a736697288f7af3
OleAndreasER <oleaegeland@gmail.com>
2022-05-26 23:06:59 +0200

    Saving logs

--- src/Arguments.hs
+++ src/Arguments.hs
@@ -6,15 +6,20 @@ import Data.Char
 answerArguments :: IO ()
 answerArguments = do
     args <- getArgs
+    addLog testLog
     putStrLn $ answer args
 
 answer :: [String] -> String
-answer ["next"] = "Squats and stuff (big weight)"
+answer ["next"] = testLog
 answer ["list", logs] = if all isDigit logs then list else answer ["x"]
     where list = concat $ replicate n "Squats\n"
           n    = read logs
-answer ["add"] = "Added Squats and stuff"
+answer ["add"] = "Added:" ++ testLog
 answer ["stats"] = "Squat 3RM: 300kg"
 answer ["bw"] = "Your bodyweight is now 100kg (real man)"
 answer ["help"] = "try next, list n, add, stats, bw"
 answer _ = "Not a real argument. Try 'endgame2 help'"
+
+testLog = "\n05/26/22:\n  PR Deadlift: 152.5kg 1/4\n  Volume Press: 48.75kg 2/3\n  Volume Chins: 16.25kg 2/5"
+
+addLog = appendFile "logs.txt"

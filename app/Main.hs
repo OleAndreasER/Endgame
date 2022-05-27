9e9f5925218663ae7ddfda00cc832d926e3afe84
OleAndreasER <oleaegeland@gmail.com>
2022-05-27 20:43:56 +0200

    Some filehandling of profiles and programs

--- app/Main.hs
+++ app/Main.hs
@@ -3,6 +3,7 @@ module Main where
 import System.Environment
 import Arguments (handleArguments)
 
+main :: IO ()
 main = do
     arguments <- getArgs
     handleArguments arguments
--- src/FileHandling.hs
+++ src/FileHandling.hs
@@ -1,10 +1,17 @@
 module FileHandling where
 
+import System.Directory (createDirectoryIfMissing)
+
 addLog :: String -> IO ()
-addLog = appendFile "logs.txt"
+addLog = appendFile "endgame-profiles/first-profile/logs.txt"
 
 nextLog :: String
 nextLog = testLog
 
 testLog :: String
 testLog = "\n05/26/22:\n  PR Deadlift: 152.5kg 1/4\n  Volume Press: 48.75kg 2/3\n  Volume Chins: 16.25kg 2/5"
+
+--A profile contains training logs and stats. 
+createProfile :: String -> IO ()
+createProfile profileName =
+    createDirectoryIfMissing True ("endgame-profiles/" ++ profileName)

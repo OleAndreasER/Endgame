5130e0200120423c5af4c2617ecf2562b7e7b7cd
OleAndreasER <oleaegeland@gmail.com>
2022-05-27 18:16:00 +0200

    Clean up

--- src/Arguments.hs
+++ src/Arguments.hs
@@ -1,31 +1,41 @@
 module Arguments where
 
 import Data.Char 
-import FileHandling (nextLog)
+import FileHandling (nextLog, addLog)
 
 handleArguments :: [String] -> IO ()
-handleArguments ["next"] = putStrLn nextLog
+handleArguments ["next"] =
+    putStrLn nextLog
 
-handleArguments ["list", amountOfLogs] = putStrLn $ listOfLatestLogs amountOfLogs
+handleArguments ["list", amountOfLogs] =
+    putStrLn $ listOfLatestLogs amountOfLogs
 
-handleArguments ["add"] = putStrLn $ "Added:" ++ nextLog
+handleArguments ["add"] = do
+    addLog nextLog
+    putStrLn $ "Added:" ++ nextLog
 
-handleArguments ["stats"] = putStrLn "Squat 3RM: 300kg"
+handleArguments ["stats"] =
+    putStrLn "Squat 3RM: 300kg"
 
-handleArguments ["bw"] = putStrLn "Your bodyweight is now 100kg (real man)"
+handleArguments ["bw"] =
+    putStrLn "Your bodyweight is now 100kg (real man)"
 
-handleArguments ["help"] = putStrLn "try next, list n, add, stats, bw"
+handleArguments ["help"] =
+    putStrLn "try next, list n, add, stats, bw"
 
-handleArguments _ = putStrLn invalidArgumentResponse
+handleArguments _ =
+    putStrLn invalidArgumentResponse
 
 invalidArgumentResponse = "Not a real argument. Try 'endgame2 help'"
 
-strIsInteger :: String -> Bool
-strIsInteger = all isDigit
-
+--For "list n" argument
 listOfLatestLogs :: String -> String
 listOfLatestLogs amountOfLogs
     | strIsInteger amountOfLogs = list
     | otherwise                 = invalidArgumentResponse
     where list = concat $ replicate n "Squats\n"
           n    = read amountOfLogs
+
+strIsInteger :: String -> Bool
+strIsInteger = all isDigit
+-- 

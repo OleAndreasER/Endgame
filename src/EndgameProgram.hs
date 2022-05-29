1313c7f899fa3d28e139ad6c55410228d77c2f5e
OleAndreasER <oleaegeland@gmail.com>
2022-05-29 18:07:52 +0200

    Better types

--- src/EndgameProgram.hs
+++ src/EndgameProgram.hs
@@ -1,13 +1,38 @@
 module EndgameProgram where
 
-data Program = Program [Cycle] [LiftCycle]
-
-data Set = Set Integer Integer Float --Sets, Reps, Percent
+data Program = Program [LiftTypeCycle] [LiftCycle]
     deriving (Show)
 
-data LiftCycle = LiftCycle String [[Set]]
+data SetType = PR | Work
+    deriving (Show, Enum)
+
+data Set = Set Integer Float SetType
     deriving (Show)
 
---Example: Squat -> Deadlift ->
-data Cycle = Cycle [String]
+data LiftSession = LiftSession SetType [Set] 
     deriving (Show)
+
+--[Squat, Deadlift]
+data LiftTypeCycle = LiftTypeCycle [String]
+    deriving (Show)
+
+--Squat cycle: [PR [(3 100 PR), (5 87 Work)],
+--              Work [(5 87 Work), (5 87 Work), (5 87 Work)] Work]
+data LiftCycle = LiftCycle String [LiftSession]
+    deriving (Show)
+
+exProgram = Program [LiftTypeCycle ["Press", "Bench"],
+                     LiftTypeCycle ["Squat", "Deadlift"],
+                     LiftTypeCycle ["Chin", "Row"]]
+                    [LiftCycle "Press"    [LiftSession PR [Set 3 100 PR, Set 5 87 Work],
+                                           LiftSession Work [Set 5 87 Work, Set 5 87 Work, Set 5 87 Work]],
+                     LiftCycle "Bench"    [LiftSession PR [Set 3 100 PR, Set 5 87 Work],
+                                           LiftSession Work [Set 5 87 Work, Set 5 87 Work, Set 5 87 Work]],   
+                     LiftCycle "Squat"    [LiftSession PR [Set 3 100 PR, Set 5 87 Work],
+                                           LiftSession Work [Set 5 87 Work, Set 5 87 Work, Set 5 87 Work]],   
+                     LiftCycle "Deadlift" [LiftSession PR [Set 3 100 PR],
+                                           LiftSession Work [Set 5 87 Work, Set 5 87 Work]],
+                     LiftCycle "Chin"     [LiftSession PR [Set 3 100 PR, Set 5 87 Work, Set 5 87 Work],
+                                           LiftSession Work [Set 5 87 Work, Set 5 87 Work, Set 5 87 Work, Set 5 87 Work]],
+                     LiftCycle "Row"      [LiftSession PR [Set 3 100 PR, Set 5 87 Work, Set 5 87 Work],
+                                           LiftSession Work [Set 5 87 Work, Set 5 87 Work, Set 5 87 Work, Set 5 87 Work]]]

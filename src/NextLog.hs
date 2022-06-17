6855f5226e48ca1cd8a93bfebb74d0af555167e4
oarorvik <oarorvik@stud.ntnu.no>
2022-06-17 18:10:13 +0200

    scary type signature

---
+++ src/NextLog.hs
@@ -0,0 +1,11 @@
+module NextLog where 
+
+import qualified EndgameLog as Log
+import qualified EndgameProgram as Program
+import EndgameStats 
+
+nextLog  :: [Log.Log]
+         -> Stats
+         -> Program.Program
+         -> (Log.Log, Stats)
+nextLog = undefined

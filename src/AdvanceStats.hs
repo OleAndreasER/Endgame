65ece5a9c856db5d0bcca4019a12906ca0fd455f
oarorvik <oarorvik@stud.ntnu.no>
2022-07-05 14:21:11 +0200

    advancing liftGroupPositions

--- src/AdvanceStats.hs
+++ src/AdvanceStats.hs
@@ -4,4 +4,14 @@ import EndgameStats
 import EndgameLog
 
 advanceStats :: Stats -> Log -> Stats
-advanceStats = undefined
+advanceStats stats log = stats
+    { liftGroupPositions = 
+        map advanceCyclePosition 
+        $ liftGroupPositions stats
+    , lifts = undefined } 
+
+advanceCyclePosition :: CyclePosition -> CyclePosition
+advanceCyclePosition (CyclePosition pos len) =
+    CyclePosition
+        { position = (pos + 1) `mod` len 
+        , EndgameStats.length = len }

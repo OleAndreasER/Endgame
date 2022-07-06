c4e4eb35a16ec0f6e148f06c3478080d1979a4e1
oarorvik <oarorvik@stud.ntnu.no>
2022-07-06 12:49:45 +0200

    File handling

--- src/FileHandling.hs
+++ src/FileHandling.hs
@@ -4,6 +4,7 @@ import System.Directory (createDirectoryIfMissing)
 import Data.Binary
 import EndgameProgram (Program)
 import EndgameLog (Log, testLog)
+import EndgameStats (Stats)
 
 
 addLog :: String -> Log -> IO ()
@@ -11,14 +12,27 @@ addLog profile log = do
     logs <- readLogs profile
     encodeFile ("endgame-profiles/"++profile++"/logs.txt") (log:logs)
 
+setStats :: String -> Stats -> IO ()
+setStats profile stats =
+    encodeFile ("endgame-profiles/"++profile++"/stats.txt") stats
+
+readStats :: String -> IO Stats
+readStats profile =
+    decodeFile ("endgame-profiles/"++profile++"/stats.txt")
+
 readLogs :: String -> IO [Log]
-readLogs profile = decodeFile ("endgame-profiles/"++profile++"/logs.txt")
+readLogs profile =
+    decodeFile ("endgame-profiles/"++profile++"/logs.txt")
 
 readProgram :: String -> IO Program
-readProgram programName = decodeFile ("endgame-programs/" ++ programName)
+readProgram programName =
+    decodeFile ("endgame-programs/"++programName)
 
---A profile contains training logs and stats. 
-createProfile :: String -> IO ()
-createProfile profile =
-    createDirectoryIfMissing True ("endgame-profiles/" ++ profile)
+--A profile contains training logs, stats and program. 
+createProfile :: String -> String -> IO ()
+createProfile profile programName = do
+    createDirectoryIfMissing True directory
+    program <- readProgram programName
+    encodeFile (directory ++ "/program.txt") program
+    where directory = "endgame-profiles/" ++ profile
 

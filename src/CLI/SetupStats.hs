770e6ab6fe504bace5d05de04fe361c87b78d008
oarorvik <oarorvik@stud.ntnu.no>
2022-07-18 11:39:18 +0200

    Got the types lined up in setupStats

--- src/CLI/SetupStats.hs
+++ src/CLI/SetupStats.hs
@@ -1,5 +1,21 @@
 module CLI.SetupStats where
 
 import Types.Stats
-import Types.Program
 
+{- Goes through each lift,
+   and asks pr, etc. 
+-}
+
+setupStats :: Stats -> IO Stats
+setupStats stats = do
+    setupBodyweight' <- setupBodyweight
+    setupLifts <- sequence $ map setupLift $ lifts stats
+    return $ stats
+        { lifts = setupLifts
+        , bodyweight = setupBodyweight' }
+
+setupLift :: LiftStats -> IO LiftStats
+setupLift = return
+
+setupBodyweight :: IO Bodyweight
+setupBodyweight = return 5

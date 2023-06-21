159202678cfeba77cfd5f67185ab54a7ef00c4b6
oarorvik <oarorvik@stud.ntnu.no>
2023-06-21 13:08:01 +0200

    Redid routes, added multiple nexts.

---
+++ src/Db/FromSchema.hs
@@ -0,0 +1,15 @@
+module Db.FromSchema
+    (
+    ) where
+
+import qualified Db.Schema as Schema
+import qualified Program.Program as Program
+import Database.Persist (Entity)
+
+program
+    :: a
+    -> Program.Program
+program = undefined
+
+
+
--- src/Server/Main.hs
+++ src/Server/Main.hs
@@ -1,18 +1,4 @@
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE EmptyDataDecls             #-}
-{-# LANGUAGE FlexibleContexts           #-}
-{-# LANGUAGE FlexibleInstances          #-}
-{-# LANGUAGE GADTs                      #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE MultiParamTypeClasses      #-}
-{-# LANGUAGE QuasiQuotes                #-}
-{-# LANGUAGE TemplateHaskell            #-}
-{-# LANGUAGE TypeFamilies               #-}
-{-# LANGUAGE DerivingStrategies #-}
-{-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE StandaloneDeriving #-}
-{-# LANGUAGE UndecidableInstances #-}
-{-# LANGUAGE DataKinds #-}
 module Server.Main
     ( startServer
     ) where
@@ -25,30 +11,16 @@ import Data.Monoid ((<>))
 import Data.Text (Text, pack)
 import File.Profile (readLog, readLogs, readProfile)
 import Control.Monad.IO.Class (liftIO)
-import Profile.NextLog (nextLog)
+import Profile.NextLog (nextLog, nextLogs)
 import Control.Monad.Logger (LoggingT, runStdoutLoggingT)
-import Database.Persist hiding (get)
-import qualified Database.Persist as P
-import Database.Persist.Sqlite hiding (get)
-import Database.Persist.TH
 
-type Api = SpockM SqlBackend () () ()
+type Api = SpockM () () () ()
 
-type ApiAction a = SpockAction SqlBackend () () a
-
-share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
-  Person json
-    name Text
-    age Int
-    deriving Show
-|]
+type ApiAction a = SpockAction () () () a
 
 startServer :: IO ()
 startServer = do
-  pool <- runStdoutLoggingT $ createSqlitePool "api.db" 5
-  runStdoutLoggingT $ runSqlPool (do runMigration migrateAll) pool
-
-  spockConfig <- defaultSpockCfg () (PCPool pool) ()
+  spockConfig <- defaultSpockCfg () PCNoDatabase ()
   runSpock 8080 (spock spockConfig app)
 
 app :: Api
@@ -56,30 +28,17 @@ app = prehook corsHeader $ do
   get "log" $ do
     log <- liftIO $ readLogs 200
     json log
-  get "next" $ do
+  get ("log" <//> "next") $ do
     nextLog' <- liftIO $ nextLog <$> readProfile
     json nextLog'
-  post "people" $ do
-      maybePerson <- jsonBody :: ApiAction (Maybe Person)
-      case maybePerson of
-        Nothing -> errorJson 1 "Failed to parse request body as Person"
-        Just thePerson -> do
-          newId <- runSQL $ insert thePerson
-          json $ object ["result" .= String "success", "id" .= newId]
-  get "people" $ do
-    allPeople <- runSQL $ selectList [] [Asc PersonId]
-    json allPeople
-
+  get ("log" <//> "next" <//> var) $ \logCount -> do
+    nextLogs' <- liftIO $ take logCount . nextLogs <$> readProfile
+    json nextLogs'
 
 corsHeader = do
     context <- getContext
     setHeader "Access-Control-Allow-Origin" "*"
     pure context
-
-runSQL
-  :: (HasSpock m, SpockConn m ~ SqlBackend)
-  => SqlPersistT (LoggingT IO) a -> m a
-runSQL action = runQuery $ \conn -> runStdoutLoggingT $ runSqlConn action conn
 
 errorJson :: Int -> Text -> ApiAction ()
 errorJson code message =
